be6a07c0fcc4d741bbd89549bc77a4ca7dae7b9b
Jarvis <jarvis.avinash@gmail.com>
2023-12-21 21:27:11 +0530

    Client (ReactJS) & Server (.NETCore)
    - Created client in ReactJS and Server in DotNetCore.
    - Also set up the connection between them using Axios

---
+++ Project/client-reactjs/src/components/Register.js
@@ -0,0 +1,46 @@
+import React from "react";
+import axios from "axios";
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
+
+  Handler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  SubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+
+    axios.post("http://localhost:5293/api/user", this.state)
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <div>
+        <div className="RegisterForm">
+          <br /><br />
+          <form onSubmit={this.SubmitHandler}>
+            <input onChange={this.Handler} value={email} placeholder="Email" name="email" type="text"></input>
+            <input onChange={this.Handler} value={password} placeholder="Password" name="password" type="text"></input>
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Register;
