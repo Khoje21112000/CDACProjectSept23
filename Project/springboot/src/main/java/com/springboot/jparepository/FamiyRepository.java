de72cf77652f50709c88e94e95a43694e3e26aa7
Karan Khoje <karanpkhojekpk@gmail.com>
2024-03-08 00:22:49 +0530

    first commit

--- Project/springboot/src/main/java/com/springboot/jparepository/FamiyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.jparepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.model.FamilyPolicy;
-
-public interface FamiyRepository extends JpaRepository<FamilyPolicy, Long>{
-
-}
